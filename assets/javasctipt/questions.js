6c9242c0598e1f698b1ef771987985ece847dad6
Skoobieton <91810317+Tanyaskubon@users.noreply.github.com>
2021-12-21 17:06:04 -0500

    add questions

---
+++ assets/javasctipt/questions.js
@@ -0,0 +1,33 @@
+var questions=[
+    {
+        title:"What does DOM stand for?",
+        choices:["Documnt Object Model", "Dog On Mountin", "Dont Move","Div tools"],
+        answer:"Document Object Model"
+        
+    },
+    {
+        title:"What is a form object closed in?",
+        choices:["<script>","<body","<form>","<a>"],
+        answer:"<form>"
+        
+    },
+    {
+        title:"Which is not a JS scope",
+        choices:["globla scope","tele scope","local scope","block scope"],
+        answer:"tele scope"
+        
+    },
+    {
+        title:"Data objects inherit properties from",
+        choices:["math objects","array objects","data objects","scope objects"],
+        answer:"data objects"
+        
+    },
+    {
+        title:"this key word refers to what",
+        choices:["var","window","scope","object"],
+        answer:"object"
+        
+    }
+]
+
